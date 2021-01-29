28fa50a87f181160465e256f0297cf7303df1ec4
Emilio Resende <Emilio.Resende@coxautoinc.com>
2021-01-29 18:49:52 -0300

    hello world

---
+++ src/main/java/fit/App.java
@@ -0,0 +1,16 @@
+package fit;
+
+/**
+ * Aqui temos nosso tradicional "Hello world"!
+ * Este é um comentário com múltiplas linhas
+ * IMPORTANTE: O nome da classe deve coincidir com o nome do arquivo ...
+ * e dentro de cada arquivo devemos ter apenas uma classe pública
+ */
+public class App {
+    // Aqui podemos também fazer comentários de uma linha só
+    // a JVM sempre vai iniciar a execução a
+    // partir da classe estática main, enviando os parâmetros para args[]
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
